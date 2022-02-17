706e2625c634cf6bac44d39dc845c80cfd8b1515
Sergei Shevchenko <79225168+SergeiCider@users.noreply.github.com>
2022-02-17 14:03:56 +0200

    Update issues.php

--- issues.php
+++ issues.php
@@ -4,7 +4,7 @@ class HelloWorld
 {
 	public function sayHello(DateTimeImutable $date): void
 	{
-		echo 'Hello, ' . $date->format33('j. n. Y');
+		echo 'Hello, ' . $date->format44('j. n. Y');
 	}
 }
 
